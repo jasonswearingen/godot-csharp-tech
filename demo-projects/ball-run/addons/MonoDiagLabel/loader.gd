78f5e793ad21f582bdb8033263c4055b1cf5db95
jasons <jasons@novaleaf.com>
2020-01-16 15:00:40 -0800

    MonoDiagLabel addon for godot

---
+++ demo-projects/ball-run/addons/MonoDiagLabel/loader.gd
@@ -0,0 +1,13 @@
+﻿tool
+extends EditorPlugin
+
+
+func _enter_tree():
+    # Initialization of the plugin goes here
+    # Add the new type with a name, a parent type, a script and an icon
+    add_custom_type("MonoDiagLabel", "CanvasLayer", preload("MonoDiagLabel.cs"), preload("res://icon.png"))
+
+func _exit_tree():
+    # Clean-up of the plugin goes here
+    # Always remember to remove it from the engine when deactivated
+    remove_custom_type("MonoDiagLabel")
