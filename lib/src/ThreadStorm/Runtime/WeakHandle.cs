ba8de0765a98eafbfb1f04570ff65d000d3b0ae8
jasons <jasons@novaleaf.com>
2020-01-16 18:27:28 -0800

    make test console app profileable

---
+++ lib/src/ThreadStorm/Runtime/WeakHandle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ThreadStorm.Runtime
+{
+	/// <summary>
+	/// Emulates a WeakReference<> but of type ```STRUCT```.
+	/// IMPORTANT:  you must call ```.Free()``` when done, otherwise leaks occur.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public struct WeakHandle<T>
+	{
+
+		public GCHandle handle;
+
+		public WeakHandle(object target)
+		{
+			handle = GCHandle.Alloc(target, GCHandleType.Weak);
+		}
+
+		public T Target
+		{
+			get { return (T)handle.Target; }
+		}
+
+		public void Free()
+		{
+			handle.Free();
+		}
+	}
+}
--- lib/src/ThreadStorm/scratch.cs
+++ lib/src/ThreadStorm/scratch.cs
@@ -8,31 +8,7 @@ using System.Runtime.InteropServices;
 
 
 
-/// <summary>
-/// Emulates a WeakReference<> but of type ```STRUCT```.
-/// IMPORTANT:  you must call ```.Free()``` when done, otherwise leaks occur.
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public struct WeakHandle<T>
-{
 
-	public GCHandle handle;
-
-	public WeakHandle(object target)
-	{
-		handle = GCHandle.Alloc(target, GCHandleType.Weak);
-	}
-
-	public T Target
-	{
-		get { return (T)handle.Target; }
-	}
-
-	public void Free()
-	{
-		handle.Free();
-	}
-}
 
 
 
