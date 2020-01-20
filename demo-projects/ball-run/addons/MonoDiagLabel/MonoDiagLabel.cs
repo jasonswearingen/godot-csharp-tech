81ecd756ad3d2ec3173234bbfbf767da6cef6a5d
jasons <jasons@novaleaf.com>
2020-01-20 11:58:43 -0800

    rework diagnostics label perf timer

--- demo-projects/ball-run/addons/MonoDiagLabel/MonoDiagLabel.cs
+++ demo-projects/ball-run/addons/MonoDiagLabel/MonoDiagLabel.cs
@@ -20,16 +20,23 @@ public class MonoDiagLabel : Godot.CanvasLayer
 		public MonoDiagLabel parent;
 
 
-		public _PerfTimer treeTimer;
+		//public _PerfTimer treeInsideTimer;
 
-		public TimeSpan lastTreeElapsed;
-		public _PerfTimer outsideTreeTimer;
+		//public TimeSpan lastTreeElapsed;
+		//public _PerfTimer treeOutsideTimer;
+
+		public _PerfTimer2 insideTreePerf = new _PerfTimer2("insideTree");
+		public _PerfTimer2 outsideTreePerf = new _PerfTimer2("outsideTree");
+
+		public TimeSpan sampleInterval;
 
 
 		public _MonoDiagLabel_TreeEndHelper(TimeSpan sampleInterval, TimeSpan historyLength)
 		{
-			treeTimer = new _PerfTimer(sampleInterval, historyLength);
-			outsideTreeTimer = new _PerfTimer(sampleInterval, historyLength);
+			//treeInsideTimer = new _PerfTimer(sampleInterval, historyLength);
+			//treeOutsideTimer = new _PerfTimer(sampleInterval, historyLength);
+
+			this.sampleInterval = sampleInterval;
 		}
 
 		public override void _Ready()
@@ -39,6 +46,9 @@ public class MonoDiagLabel : Godot.CanvasLayer
 			//make update last
 			this.ProcessPriority = int.MaxValue;
 
+			//this.treeInsideTimer.Restart();
+			//this.treeOutsideTimer.Restart();
+
 		}
 		public override void _Process(float delta)
 		{
@@ -46,19 +56,336 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 			//exec at tree end
 
-			treeTimer.Pause();
-			treeTimer.Lap();
+			//treeInsideTimer.Pause();
+			//treeInsideTimer.Lap();
 
-			outsideTreeTimer.Unpause();
+			//treeOutsideTimer.Unpause();
+
+			insideTreePerf.EndSample();
+			outsideTreePerf.BeginSample();
 		}
 
 
 		public void OnTreeProcessStart()
 		{
-			outsideTreeTimer.Pause();
-			outsideTreeTimer.Lap();
+			//treeOutsideTimer.Pause();
+			//treeOutsideTimer.Lap();
 
-			treeTimer.Unpause();
+			//treeInsideTimer.Unpause();
+
+			outsideTreePerf.EndSample();
+			insideTreePerf.BeginSample();
+
+		}
+
+
+	}
+
+	public static class _Stats
+	{
+		public struct Quartiles<T>
+		{
+			public T q0;
+			public T q1;
+			public T q2;
+			public T q3;
+			public T q4;
+
+			public int count;
+
+			public override string ToString()
+			{
+				return $"{q0} / {q1} / {q2}  / {q3} / {q4}  ({count} samples)";
+			}
+		}
+
+		/// <summary>
+		/// Return the quartile values of an ordered set of doubles
+		///   assume the sorting has already been done.
+		///   
+		/// This actually turns out to be a bit of a PITA, because there is no universal agreement 
+		///   on choosing the quartile values. In the case of odd values, some count the median value
+		///   in finding the 1st and 3rd quartile and some discard the median value. 
+		///   the two different methods result in two different answers.
+		///   The below method produces the arithmatic mean of the two methods, and insures the median
+		///   is given it's correct weight so that the median changes as smoothly as possible as 
+		///   more data ppints are added.
+		///    
+		/// This method uses the following logic:
+		/// 
+		/// ===If there are an even number of data points:
+		///    Use the median to divide the ordered data set into two halves. 
+		///    The lower quartile value is the median of the lower half of the data. 
+		///    The upper quartile value is the median of the upper half of the data.
+		///    
+		/// ===If there are (4n+1) data points:
+		///    The lower quartile is 25% of the nth data value plus 75% of the (n+1)th data value.
+		///    The upper quartile is 75% of the (3n+1)th data point plus 25% of the (3n+2)th data point.
+		///    
+		///===If there are (4n+3) data points:
+		///   The lower quartile is 75% of the (n+1)th data value plus 25% of the (n+2)th data value.
+		///   The upper quartile is 25% of the (3n+2)th data point plus 75% of the (3n+3)th data point.
+		/// 
+		/// </summary>
+		static public Quartiles<float> ComputeQuartiles(Span<float> afVal, int length)
+		{
+			int iSize = length;
+			int iMid = iSize / 2; //this is the mid from a zero based index, eg mid of 7 = 3;
+
+			var toReturn = new Quartiles<float>();
+
+			toReturn.count = length;
+			//q0 and q4
+			toReturn.q0 = afVal[0];
+			toReturn.q4 = afVal[length - 1];
+
+			if (iSize % 2 == 0)
+			{
+				//================ EVEN NUMBER OF POINTS: =====================
+				//even between low and high point
+				toReturn.q2 = (afVal[iMid - 1] + afVal[iMid]) / 2;
+
+				int iMidMid = iMid / 2;
+
+				//easy split 
+				if (iMid % 2 == 0)
+				{
+					toReturn.q1 = (afVal[iMidMid - 1] + afVal[iMidMid]) / 2;
+					toReturn.q3 = (afVal[iMid + iMidMid - 1] + afVal[iMid + iMidMid]) / 2;
+				}
+				else
+				{
+					toReturn.q1 = afVal[iMidMid];
+					toReturn.q3 = afVal[iMidMid + iMid];
+				}
+			}
+			else if (iSize == 1)
+			{
+				//================= special case, sorry ================
+				toReturn.q1 = afVal[0];
+				toReturn.q2 = afVal[0];
+				toReturn.q3 = afVal[0];
+			}
+			else
+			{
+				//odd number so the median is just the midpoint in the array.
+				toReturn.q2 = afVal[iMid];
+
+				if ((iSize - 1) % 4 == 0)
+				{
+					//======================(4n-1) POINTS =========================
+					int n = (iSize - 1) / 4;
+					toReturn.q1 = (afVal[n - 1] * .25f) + (afVal[n] * .75f);
+					toReturn.q3 = (afVal[3 * n] * .75f) + (afVal[3 * n + 1] * .25f);
+				}
+				else if ((iSize - 3) % 4 == 0)
+				{
+					//======================(4n-3) POINTS =========================
+					int n = (iSize - 3) / 4;
+
+					toReturn.q1 = (afVal[n] * .75f) + (afVal[n + 1] * .25f);
+					toReturn.q3 = (afVal[3 * n + 1] * .25f) + (afVal[3 * n + 2] * .75f);
+				}
+			}
+
+			return toReturn;
+		}
+
+		/// <summary>
+		/// returns quartiles (0th, 1st, 2nd, 3rd, 4th, 5th.)
+		/// 
+		/// see: https://en.wikipedia.org/wiki/Quantile#Examples
+		/// 
+		/// This generic overload returns discrete values:  the items at those quartile indicies rounded down.  (no averaging of values)
+		/// the float based overload will average results.
+		/// </summary>
+		static public Quartiles<T> ComputeQuartiles<T>(Span<T> afVal, int length)
+		{
+			int iSize = length;
+			int iMid = iSize / 2; //this is the mid from a zero based index, eg mid of 7 = 3;
+
+			var toReturn = new Quartiles<T>();
+
+			toReturn.count = length;
+			//q0 and q4
+			toReturn.q0 = afVal[0];
+			toReturn.q4 = afVal[length - 1];
+
+			toReturn.q2 = afVal[iMid];
+
+			int iMidMid = iMid / 2;
+			toReturn.q1 = afVal[iMidMid];
+			toReturn.q3 = afVal[iMid + iMidMid];
+
+
+
+			return toReturn;
+
+
+		}
+	}
+
+	public class _PerfTimer2
+	{
+
+
+		private struct PerfInfo : IComparable<PerfInfo>
+		{
+			public TimeSpan sample;
+			public TimeSpan wall;
+
+			public int CompareTo(PerfInfo other)
+			{
+				return (int)(sample.Ticks - other.sample.Ticks);
+			}
+
+			public override string ToString()
+			{
+				return $"sample={sample}, wall={wall}";
+			}
+		}
+		/// <summary>
+		/// tracks wall time, used for getting history over the last N seconds
+		/// </summary>
+		private Stopwatch wallTimer = new Stopwatch();
+		/// <summary>
+		/// tracks time inside a sample
+		/// </summary>
+		private Stopwatch sampleTimer = new Stopwatch();
+
+		private Queue<PerfInfo> history;
+
+		private int maxHistory;
+
+		private string name;
+		public _PerfTimer2(string name, int maxHistory = 1000)
+		{
+			this.name = name;
+			this.maxHistory = maxHistory;
+			this.history = new Queue<PerfInfo>(maxHistory);
+			this._tempHist = new PerfInfo[maxHistory];
+			wallTimer.Start();
+		}
+
+		public void BeginSample()
+		{
+			sampleTimer.Restart();
+		}
+
+		public TimeSpan EndSample()
+		{
+			sampleTimer.Stop();
+			var toReturn = sampleTimer.Elapsed;
+
+			//keep our hist under max size
+			while (history.Count >= maxHistory)
+			{
+				history.Dequeue();
+			}
+			history.Enqueue(new PerfInfo() { sample = toReturn, wall = wallTimer.Elapsed });
+
+			return toReturn;
+
+		}
+		public void Pause()
+		{
+			sampleTimer.Stop();
+		}
+
+		public void Unpause()
+		{
+			sampleTimer.Start();
+		}
+
+		/// <summary>
+		/// stores in chrono order (first inserted to last inserted)
+		/// </summary>
+		private PerfInfo[] _tempHist;
+		public _Stats.Quartiles<TimeSpan> GetHistory(int samples)
+		{
+			var actualSamples = Math.Min(history.Count, samples);
+			return _GetHistoryHelper(0, actualSamples);
+		}
+
+
+		private _Stats.Quartiles<TimeSpan> _GetHistoryHelper(int startIndex, int length)
+		{
+			if (length == 0)
+			{
+				return default;
+			}
+			history.CopyTo(_tempHist, 0);
+			Array.Reverse(_tempHist, 0, history.Count);  //only referse length coppied by previous line
+																									 //var actualSamples = Math.Min(history.Count, samples);
+			Array.Sort(_tempHist, startIndex, length);
+
+			Span<TimeSpan> _tempFloats = stackalloc TimeSpan[length];
+			var endIndex = startIndex + length;
+			var tempIndex = 0;
+			for (var i = startIndex; i < endIndex; i++)
+			{
+				_tempFloats[tempIndex] = _tempHist[i].sample;
+				tempIndex++;
+			}
+
+			var toReturn = _Stats.ComputeQuartiles(_tempFloats, length);
+
+
+			return toReturn;
+
+		}
+
+		public _Stats.Quartiles<TimeSpan> GetHistory(TimeSpan wallInterval, TimeSpan startingFrom = default)
+		{
+
+
+			history.CopyTo(_tempHist, 0);
+			Array.Reverse(_tempHist, 0, history.Count); //only referse length coppied by previous line
+
+			var now = wallTimer.Elapsed;
+
+			var startIndexTime = now - startingFrom;
+			var endIndexTime = startIndexTime - wallInterval; //working our way backwards through time
+
+
+			//get indexes for stard/end
+			var startIndex = 0;
+			var length = 0;
+			for (var i = 0; i < history.Count; i++)
+			{
+				var current = _tempHist[i];
+				if (current.wall > startIndexTime)
+				{
+					startIndex++;
+					continue;
+				}
+
+				if (current.wall < endIndexTime)
+				{
+					break;
+				}
+				length++;
+			}
+
+			return _GetHistoryHelper(startIndex, length);
+
+		}
+
+		public string GetHistoryString(int samples)
+		{
+			var quart = GetHistory(samples);
+			return QuartileToString(quart);
+		}
+
+		public string GetHistoryString(TimeSpan wallInterval, TimeSpan startingFrom = default)
+		{
+			var quart = GetHistory(wallInterval, startingFrom);
+			return QuartileToString(quart);
+		}
+
+		public string QuartileToString(_Stats.Quartiles<TimeSpan> hist)
+		{
+			return $"{name} Quantiles: {hist.q0.TotalMilliseconds.ToString("F1") } / {hist.q1.TotalMilliseconds.ToString("F1") } / {hist.q2.TotalMilliseconds.ToString("F1") } / {hist.q3.TotalMilliseconds.ToString("F1") } / {hist.q4.TotalMilliseconds.ToString("F1") } / ({hist.count} samples)";
 		}
 
 
@@ -79,7 +406,7 @@ public class MonoDiagLabel : Godot.CanvasLayer
 		int count;
 		TimeSpan total;
 
-		public List<(TimeSpan min, TimeSpan max, int count, TimeSpan total)> history = new List<(TimeSpan min, TimeSpan max, int count, TimeSpan total)>();
+		public List<(TimeSpan min, TimeSpan max, int count, TimeSpan total, TimeSpan pause)> history = new List<(TimeSpan min, TimeSpan max, int count, TimeSpan total, TimeSpan pause)>();
 
 		public _PerfTimer(TimeSpan sampleInterval, TimeSpan historyLength)
 		{
@@ -87,8 +414,11 @@ public class MonoDiagLabel : Godot.CanvasLayer
 			this.historyLength = historyLength;
 		}
 
+		Stopwatch pauseTimer = new Stopwatch();
+
 		public void Restart()
 		{
+			pauseTimer.Reset();
 			timer.Restart();
 			min = TimeSpan.Zero;
 			max = TimeSpan.Zero;
@@ -98,11 +428,13 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 		public void Pause()
 		{
-			timer.Stop();
+			pauseTimer.Start();
+			//timer.Stop();
 		}
 		public void Unpause()
 		{
-			timer.Start();
+			//timer.Start();
+			pauseTimer.Stop();
 		}
 
 		public void Lap()
@@ -123,22 +455,24 @@ public class MonoDiagLabel : Godot.CanvasLayer
 				_ArchiveHelper();
 				Restart();
 			}
+
 		}
 
 		private void _ArchiveHelper()
 		{
 			var maxSamples = (int)(historyLength.Ticks / measureInterval.Ticks);
-			history.Insert(0, (min, max, count, total));
+			history.Insert(0, (min, max, count, total: total, pause: pauseTimer.Elapsed));
 			if (history.Count > maxSamples)
 			{
 				history.RemoveRange(maxSamples, history.Count - maxSamples);
 			}
 		}
 
-		public (TimeSpan min, TimeSpan max, int count, TimeSpan total) Report(TimeSpan targetInterval)
+		public (TimeSpan min, TimeSpan max, int count, TimeSpan total, TimeSpan pause) Report(TimeSpan targetInterval)
 		{
-			(TimeSpan min, TimeSpan max, int count, TimeSpan total) toReturn = default;
+			(TimeSpan min, TimeSpan max, int count, TimeSpan total, TimeSpan pause) toReturn = default;
 			var index = 0;
+
 			while (toReturn.total < targetInterval && history.Count > index)
 			{
 				var sample = history[index];
@@ -152,9 +486,10 @@ public class MonoDiagLabel : Godot.CanvasLayer
 				}
 				toReturn.count += sample.count;
 				toReturn.total += sample.total;
+				toReturn.pause += sample.pause;
 				index++;
 			}
-
+			toReturn.total -= toReturn.pause;
 			return toReturn;
 		}
 
@@ -162,10 +497,10 @@ public class MonoDiagLabel : Godot.CanvasLayer
 		{
 			return ReportToString(this.Report(targetInterval));
 		}
-		public static string ReportToString((TimeSpan min, TimeSpan max, int count, TimeSpan total) tuple)
+		public static string ReportToString((TimeSpan min, TimeSpan max, int count, TimeSpan total, TimeSpan pause) tuple)
 		{
 			var tupAvg = tuple.total.TotalMilliseconds / tuple.count;
-			return $"Ms Min/Avg/Max/Tot= {tuple.min.TotalMilliseconds.ToString("F2")} / {tupAvg.ToString("F2")} / {tuple.max.TotalMilliseconds.ToString("F2")} / {tuple.total.TotalMilliseconds.ToString("F2")} ({tuple.count} samples)";
+			return $"Ms Min/Avg/Max/Tot= {tuple.min.TotalMilliseconds.ToString("F2")} / {tupAvg.ToString("F2")} / {tuple.max.TotalMilliseconds.ToString("F2")} / {tuple.total.TotalMilliseconds.ToString("F2")} ({tuple.count} samples, {tuple.pause.TotalMilliseconds.ToString("F2")} paused)";
 		}
 	}
 	public enum POSITION
@@ -178,7 +513,10 @@ public class MonoDiagLabel : Godot.CanvasLayer
 	public int margin = 5;
 
 	[Export]
-	public int updatesPerSecond = 2;
+	public float labelUpdatesPerSecond = 0.5f;
+
+	[Export]
+	public float samplesPerSecond = 2;
 
 	[Export]
 	public bool showFrameInfo = true;
@@ -188,7 +526,7 @@ public class MonoDiagLabel : Godot.CanvasLayer
 	public bool showGcInfo = true;
 
 	public System.Diagnostics.Stopwatch sw;
-	public TimeSpan updateFrequency;
+	public TimeSpan labelUpdateFrequency;
 	public TimeSpan historyLength;
 
 	private StringBuilder sb = new StringBuilder(500);
@@ -212,13 +550,13 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 
 		//set our update frequency
-		this.updateFrequency = TimeSpan.FromMilliseconds(1000 / updatesPerSecond);
-		this.historyLength = TimeSpan.FromSeconds(updateFrequency.TotalSeconds * 10);
+		this.labelUpdateFrequency = TimeSpan.FromSeconds(1 / labelUpdatesPerSecond);
+		this.historyLength = TimeSpan.FromSeconds(labelUpdateFrequency.TotalSeconds * 10);
 		this.sw = System.Diagnostics.Stopwatch.StartNew();
 
 		//this.renderTime.timer = new Stopwatch();
-		framePerf = new _PerfTimer(updateFrequency, historyLength);
-		framePerf.Restart();
+		//framePerf = new _PerfTimer(updateFrequency, historyLength);
+		//framePerf.Restart();
 
 		isInitialized = true;
 		this.ProcessPriority = int.MinValue;
@@ -230,7 +568,10 @@ public class MonoDiagLabel : Godot.CanvasLayer
 	//private System.Diagnostics.Stopwatch renderTimer = new System.Diagnostics.Stopwatch();
 
 
-	private _PerfTimer framePerf;
+	//private _PerfTimer framePerf;
+
+	private _PerfTimer2 totalFramePerf = new _PerfTimer2("totalFrame");
+
 
 	//private (Stopwatch timer, TimeSpan min, TimeSpan max, int count, TimeSpan total) renderTime;
 
@@ -244,10 +585,13 @@ public class MonoDiagLabel : Godot.CanvasLayer
 			return;
 		}
 
+		totalFramePerf.EndSample();
+		totalFramePerf.BeginSample();
+
 		if (treeEndHelper == null)
 		{
 			//so we can track in-tree vs out-tree times
-			treeEndHelper = new _MonoDiagLabel_TreeEndHelper(updateFrequency, historyLength);
+			treeEndHelper = new _MonoDiagLabel_TreeEndHelper(labelUpdateFrequency, historyLength);
 			this.GetTree().Root.AddChild(treeEndHelper);
 			return;
 		}
@@ -258,7 +602,7 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 
 
-		framePerf.Lap();
+		//framePerf.Lap();
 
 		////store info in min/max frame time
 		//    var frameElapsed = renderTime.timer.Elapsed;
@@ -276,7 +620,7 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 
 
-		if (this.sw.Elapsed >= this.updateFrequency)
+		if (this.sw.Elapsed >= this.labelUpdateFrequency)
 		{
 			this.sw.Restart();
 			_setLabelText();
@@ -288,9 +632,6 @@ public class MonoDiagLabel : Godot.CanvasLayer
 			//renderTime.total = TimeSpan.Zero;
 
 		}
-
-
-
 
 	}
 
@@ -353,7 +694,12 @@ public class MonoDiagLabel : Godot.CanvasLayer
 
 		if (showFrameInfo)
 		{
-			sb.Append($"FPS ={ Engine.GetFramesPerSecond()}  FrameDetail= {framePerf.ReportToString(this.updateFrequency)}");
+			sb.Append($"FPS ={ Engine.GetFramesPerSecond()}");//  FrameDetail= {framePerf.ReportToString(this.updateFrequency)}");
+			sb.Append($"\n{totalFramePerf.GetHistoryString(this.labelUpdateFrequency)}");
+			sb.Append($"\n{treeEndHelper.insideTreePerf.GetHistoryString(this.labelUpdateFrequency)}");
+			sb.Append($"\n{treeEndHelper.outsideTreePerf.GetHistoryString(this.labelUpdateFrequency)}");
+			sb.Append($"\n");
+			//sb.Append($"\nInside Tree ={treeEndHelper.treeInsideTimer.ReportToString(this.updateFrequency)}  Outside Tree ={treeEndHelper.treeOutsideTimer.ReportToString(this.updateFrequency)}");
 			//var min = renderTime.min.TotalMilliseconds.ToString("F1");
 			//var avg = (renderTime.total.TotalMilliseconds / renderTime.count).ToString("F1");
 			//var max = renderTime.max.TotalMilliseconds.ToString("F1");
