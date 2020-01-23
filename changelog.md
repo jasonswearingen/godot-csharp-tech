4bdff852e363c2338a19d95058e08a5a09ab8674
jasons <jasons@novaleaf.com>
2020-01-23 13:27:04 -0800

    modify file structure so that we can have a single source for addons  example MonoDiagLabel

---
+++ changelog.md
@@ -0,0 +1,4 @@
+
+# 20200123
+- started tracking enhancements in this changelog
+- MonoDiagLabel: new perfTimer code used, displays quantiles for improved diagnostics.
---
+++ demo-projects/scratch/addons/godot-csharp-tech/shims/MonoDiagLabel.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Godot;
+
+/// <summary>
+/// your .csproj must link the "godot-csharp-tech" library to use this
+/// </summary>
+[Tool]
+public class MonoDiagLabel : godot_csharp_tech.addons.MonoDiagLabel
+{ 
+}
